1d7adf6a3a1e4b6106edc8f150ba38b292eefe82
Ada Lynn <adadatalynn@gmail.com>
2026-05-10 15:41:46 -0500

    vite works!!!

--- vagabond-lite.js
+++
@@ -1,4 +0,0 @@
-// only required for dev
-// in prod, foundry loads vagabond-lite.mjs, which is compiled by vite/rollup
-// in dev, foundry loads vagabond-lite.mjs, this file, which loads vagabond-lite.ts
-import './src/vagabond-lite.ts';
