67b4715810fa954f9b3a24d26c705c4901ec9dcf
DESKTOP-PC\Terra <terra.shaide@gmail.com>
2026-05-13 15:20:03 -0500

    Hero inventory now contains a 'container' instead of its own list of items and slot max.

--- src/model/actor/type/Inventory.ts
+++ src/model/actor/type/Inventory.ts
@@ -1,15 +1,15 @@
 import { coinSchema, CoinValue, consolidate } from "../../common/CoinValue"
 import { fields, requiredInteger } from "../../common/sharedSchemas"
-import EquipmentDataModel from "../../item/equip/EquipmentDataModel"
+import ContainerDataModel from "../../item/equip/ContainerDataModel"
+import EquipmentDataModel, { EquipmentSchema } from "../../item/equip/EquipmentDataModel"
 import HeroDataModel from "../HeroDataModel"
 
 export const inventorySchema = () => {
     return {
         coins: new fields.SchemaField({ ...coinSchema() }),
         occupiedSlots: new fields.NumberField({ ...requiredInteger, initial: 0 }),
-        maxSlots: new fields.NumberField({ integer: true, min: 8, initial: 8 }),
+        container: new fields.SchemaField({ ...ContainerDataModel.defineSchema() }),
         slotBonus: new fields.NumberField({ integer: true, min: 0, initial: 0 }),
-        items: new fields.ArrayField( new fields.SchemaField({ ...EquipmentDataModel.defineSchema() }), { initial: [] } ),
         equipped: new fields.ArrayField(
             new fields.SchemaField({ ...EquipmentDataModel.defineSchema() })
         )
@@ -21,6 +21,6 @@ export type Inventory = foundry.abstract.TypeDataModel<InventorySchema, any>
 
 export function setInventoryData(hero: HeroDataModel) {
     consolidate(hero.inventory.coins as CoinValue)
-    hero.inventory.items.forEach((i) => hero.inventory.occupiedSlots! += i.slots!)
-    hero.inventory.maxSlots = Number(hero.stats.might) + 8 + hero.inventory.slotBonus! - hero.fatigue
+    hero.inventory.container.items.forEach((i) => hero.inventory.occupiedSlots! += i.slots!)
+    hero.inventory.container.size = Number(hero.stats.might) + 8 + hero.inventory.slotBonus! - hero.fatigue
 }
