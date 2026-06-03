5f6baadced4ceea8b5296030d044146e29be1823
DESKTOP-PC\Terra <terra.shaide@gmail.com>
2026-06-03 15:38:56 -0500

    Fixed an issue where tracker values would "nudge" surrounding elements as character width changed.

--- src/view/sheets/actor/hero/tab/TopSection.tsx
+++ src/view/sheets/actor/hero/tab/TopSection.tsx
@@ -98,7 +98,7 @@ export const Trackers = ({ hero }: { hero: HeroDataModel }) => {
     const trackerClasses = `flex gap-1 items-center`
 
     return (
-        <div className="w-full flex flex-col items-center">
+        <div className="w-full flex flex-col justify-between ml-4 mr-4">
             <Header title={lang.VGLITE.HeroSheet.trackers} />
             <div className="flex gap-3">
                 <Tracker
@@ -119,9 +119,9 @@ export const Trackers = ({ hero }: { hero: HeroDataModel }) => {
 }
 
 const Tracker = ({ name, content, onClick }: { name: string, content: ReactNode, onClick: (auxClick: boolean) => void }) => (
-    <div className="flex items-center flex-col text-text-primary" onClick={() => onClick(false)} onAuxClick={() => onClick(true)}>
+    <div className="flex items-center flex-col text-text-primary w-1/3" onClick={() => onClick(false)} onAuxClick={() => onClick(true)}>
         {name}
-        <span className="font-eskapade font-bold text-2xl -mt-1">{content}</span>
+        <span className="font-eskapade font-bold text-2xl -mt-1 ">{content}</span>
     </div>
 )
 
