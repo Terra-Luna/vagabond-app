b755a2e23384d6a746e56cfb9a4089c987ce56e6
DESKTOP-PC\Terra <terra.shaide@gmail.com>
2026-06-17 16:41:08 -0500

    Changing directions with adversary sheet.

--- src/view/sheets/actor/adversary/AdversarySheet.tsx
+++ src/view/sheets/actor/adversary/AdversarySheet.tsx
@@ -1,22 +1,20 @@
+    import { useCallback } from "react"
 import lang from "../../../../../public/lang/en.json"
 import AdversaryDataModel from "../../../../model/actor/AdversaryDataModel"
-import { localizeString } from "../../../../utils/localeUtils"
-import { EditableNameField } from "../../../component/EditableTextField"
+import { EditableNameField, EditableTextField } from "../../../component/EditableTextField"
 import { Divider } from "../../../component/Header"
+import { Portrait } from "../hero/HeroSheet"
 import { FoundryActor, VgLiteActorSheet } from "../VgLiteActorSheet"
+import { updateDocument } from "../../../../utils/documentUtils"
 
 const locale = lang.VGLITE.AdversarySheet
-const cardHeaderLayout = "flex items-center pt-2 pb-1 pl-2 pr-2 bg-section-header-fill"
-const cardHeaderStyle = "text-text-section-header text-2xl font-eskapade font-bold"
-const cardSubheaderLayout = "flex items-center border-r-1 border-solid border-table-border"
-const cardSubheaderStyle = "flex text-text-section-header text-sm pb-1 pl-2 pr-8 bg-section-header-fill [clip-path:polygon(0_0,100%_0,90%_100%,0_100%)]"
 
 export default class AdversarySheet extends VgLiteActorSheet {
     Component = AdversarySheetReactComponent
     static DEFAULT_OPTIONS = {
         position: {
-            width: 440,
-            height: 360
+            width: 420,
+            height: 300
         },
         window: {
             resizable: false
@@ -26,79 +24,53 @@ export default class AdversarySheet extends VgLiteActorSheet {
 
 const AdversarySheetReactComponent = ({ actor, sheet }: { actor: FoundryActor<AdversaryDataModel>, sheet: VgLiteActorSheet }) => {
     const adv = actor.system
-    return (<>
-        <div className={`${cardHeaderLayout} ${cardHeaderStyle}`}>
-            <EditableNameField actor={actor} />
-            <Divider />
-            <p className="text-sm">
-                {localizeString(locale.tl, { tl: adv.threatLevel?.toString() ?? '' })}
-            </p>
+    return (
+        <div className="@container flex flex-col grow">
+            <AdversarySheetHeader adv={adv} />
+            <Description adv={adv} />
+            <StatBlock adv={adv} />
         </div>
-        <div className={cardSubheaderLayout}>
-            <div className={cardSubheaderStyle}>
-                {`${lang.VGLITE.Sizes[adv.beingSize]} • ${lang.VGLITE.BeingTypes[adv.beingType]}`}
-            </div>
-        </div>
-        <AdversaryCardBody adv={adv}  />
-    </>)
+    )
 }
 
-const AdversaryCardBody = ({ adv }: { adv: AdversaryDataModel }) => {
+const AdversarySheetHeader = ({ adv }: { adv: AdversaryDataModel }) => {
     return (
-        <div className="mx-2">
-            <Description adv={adv} />
-            <Stats adv={adv} />
-            <Actions adv={adv} />
-            <Abilities adv={adv} />
+        <div className="flex">
+            <Portrait actor={adv} />
+            <div className="bg-sheet-header-fill font-eskapade grow ml-[123px]">
+                <div className="text-text-header-primary text-2xl font-bold mt-1 ml-2 flex">
+                    <EditableNameField actor={adv.parent} />
+                    <div className="flex ml-auto mr-2 mt-1 text-md">
+                        <span>{locale.hd}&nbsp;</span>
+                        <EditableTextField
+                            initialValue={adv.hitDice?.toString() ?? '1'}
+                            updateProps={{ actor: adv.parent, propertyPath: ['hitDice'] }}
+                        />
+                    </div>
+                </div>
+                <div className="flex text-text-header-secondary ml-2 pb-1">
+                    <span>{`${lang.VGLITE.Sizes[adv.beingSize]} • ${lang.VGLITE.BeingTypes[adv.beingType]}`}</span>
+                    <div className="ml-auto mr-2">
+                        <span className="font-eskapade font-bold text-md">{`${locale.tl} ${adv.threatLevel}`}</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
 
 const Description = ({ adv }: { adv: AdversaryDataModel }) => {
     return (
-        <div className="border-2 border-dotted border-transparent border-b-section-header-fill py-1 italic">
+        <div className="ml-[123px] py-1 italic border-2 border-dotted border-transparent border-b-section-header-fill">
             {adv.description + "A relentless hunter of living air, capable of directly locating any scent that lingers in the winds."}
         </div>
     )
 }
 
-const Stats = ({ adv }: { adv: AdversaryDataModel }) => {
-    return (
-        <div className="flex-col">
-            {localizeString(locale.hd, { hd: adv.hitDice?.toString() ?? '1' })}
-            {localizeString(locale.zone, { zone: adv.zone?.toString() ?? '' })}
-            {localizeString(locale.senses, { senses: adv.senses.toString() ?? '' })}
-            {localizeString(locale.armor, { rating: adv.armor.rating?.toString() ?? '', type: adv.armor.as?.toString() ?? 'Unarmored' })}
-            {localizeString(locale.immune, { immunities: '' })}
-            {localizeString(locale.weak, { weaknesses: '' })}
-            {localizeString(locale.status_immunities, { immunities: '' })}
-
-            {localizeString(locale.speed, { spd: adv.movement?.toString() ?? '30' })}
-            {localizeString(locale.appearing, { num: adv.numberAppearing?.toString() ?? '1' })}
-            {localizeString(locale.morale, { morale: adv.morale?.toString() ?? '0' })}
-
-        </div>
-    )
-}
-
-const Actions = ({ adv }: { adv: AdversaryDataModel }) => {
+const StatBlock = ({ adv }: { adv: AdversaryDataModel }) => {
     return (
         <div>
-            <span className="text-lg font-eskapade font-bold">
-                Actions
-                <Divider />
-            </span>
-        </div>
-    )
-}
 
-const Abilities = ({ adv }: { adv: AdversaryDataModel }) => {
-    return (
-        <div>
-            <span className="text-lg font-eskapade font-bold">
-                Abilities
-                <Divider />
-            </span>
         </div>
     )
 }
