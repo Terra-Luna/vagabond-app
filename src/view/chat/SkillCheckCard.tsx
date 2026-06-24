32ef200cb494f9994554238225d9346b8236b512
DESKTOP-PC\Terra <terra.shaide@gmail.com>
2026-06-24 15:41:49 -0500

    Skill check chat card progress. Removed r-click since there's a trash can icon

--- src/view/chat/SkillCheckCard.tsx
+++ src/view/chat/SkillCheckCard.tsx
@@ -4,37 +4,36 @@ import { useContextMenu } from '../component/ContextMenu'
 import { Minus, Plus, Trash } from 'lucide-react'
 import { tableBorder } from '../common/border-styles'
 import { DiceRoll } from './DiceRoll'
+import { glowOnHover } from "../sheets/VgLiteSheet"
 
 const chatCardBodyStyle = `${tableBorder} rounded-md text-text-primary text-lg font-eskapade font-bold bg-sheet-main-fill p-2`
 
 export const SkillCheckChatCard = ({ portrait, result }: { portrait: string, result: SkillCheckResult }) => {
-    const { onCtxMenu, ContextMenu } = useContextMenu()
+    //const { onCtxMenu, ContextMenu } = useContextMenu()
+    const [resultTextColor, resultBorderColor] = result.result === lang.VGLITE.RollResult.failure ? ['text-failure', 'border-failure'] : ['text-success', 'border-success']
     return (
         <div
             className={chatCardBodyStyle}
-            onContextMenu={(e) => onCtxMenu(e, [
+            /* onContextMenu={(e) => onCtxMenu(e, [
                 { icon: Trash, label: 'Delete', action: () => { console.log("Delete - clicked!") }, isDestructive: true }
-            ])}>
+            ])} */>
             <div>
-                <ResultBanner portrait={portrait} title={result.skillName} csf={result.result} />
+                <ResultBanner portrait={portrait} title={result.skillName} borderColor={resultBorderColor} />
                 <DiceGraphics d20={result.d20} d6={result.d6} favHinder={result.favorHinder} />
-                <TotalsFooter total={result.total} difficulty={result.difficulty} />
+                <TotalsFooter total={result.total} difficulty={result.difficulty} csf={result.result} resultTextColor={resultTextColor} />
             </div>
-            <ContextMenu />
+            {/* <ContextMenu /> */}
         </div>
     )
 }
 
-const ResultBanner = ({ portrait, title, csf }: { portrait: string, title: string, csf: string }) => {
-    const [resultTextColor, resultBorderColor] = csf === lang.VGLITE.RollResult.failure ?
-        ['text-failure', 'border-failure'] : ['text-success', 'border-success']
-    const border = `border border-solid ${resultBorderColor} rounded-md p-1`
+const ResultBanner = ({ portrait, title, borderColor }: { portrait: string, title: string, borderColor: string }) => {
+    const border = `border border-solid ${borderColor} rounded-sm p-1`
     return (
         <div className={`flex space-x-1 items-center bg-sheet-header-fill ${border}`}>
             <img className={`object-contain h-[54px] w-[54px]`} src={portrait} alt={'hero'} />
             <div className="text-text-header-primary">
-                <div className="text-xl">{title} Check</div>
-                <div className={`${resultTextColor}`}>{csf}</div>
+                <div className="text-2xl mr-1">{title} Check</div>
             </div>
         </div>
     )
@@ -59,13 +58,14 @@ const DiceGraphics = ({ d20, d6, favHinder }) => {
     )
 }
 
-const TotalsFooter = ({ total, difficulty }) => {
+const TotalsFooter = ({ total, difficulty, csf, resultTextColor }) => {
     return (
         <div className="flex mt-2 space-x-2 h-fit items-end">
             <p className="font-paradigm font-normal">Total:</p>
             <p className="text-4xl mr-4">{total}</p>
             <p className="font-paradigm font-normal">vs:</p>
             <p className="text-xl">{difficulty}</p>
+            <p className={`${resultTextColor} text-xl ml-auto mr-2 [text-shadow:0_0_10px_var(--color-text-glow)]`}>{csf}</p>
         </div>
     )
 }
