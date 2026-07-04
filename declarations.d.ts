b83b823ea475ad75e877c2dc2c022834d8ffeed5
Ada Lynn <adadatalynn@gmail.com>
2026-07-04 13:08:55 -0500

    GET IN BOYS WE'RE HOT RELOADING

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,3 +1,8 @@
+declare module "*.css" {
+  const content: any;
+  export default content;
+}
+
 declare module '*.svg?react' {
   import { FunctionComponent } from "react";
   const node: FunctionComponent<{ className: string, fill: any }>;
--- src/utils/styleUtils.ts
+++ src/utils/styleUtils.ts
@@ -1,4 +1,25 @@
-import { ReactNode } from "react"
-import styles from "../styles/vagabond-lite.css?inline"
+import "../styles/vagabond-lite.css"
 
-export const vgLiteStyles = styles as unknown as ReactNode
+const documentWithStyleTag = (document as any) as { vgLiteDevStyleSheet: HTMLStyleElement }
+
+const removeAndSaveVgLiteStyleTag = () => {
+    const vgLiteStyleTag = document.querySelector('style[data-vite-dev-id*="vagabond-lite.css"]') as HTMLStyleElement;
+    if (vgLiteStyleTag) {
+        vgLiteStyleTag.remove();
+        documentWithStyleTag.vgLiteDevStyleSheet = vgLiteStyleTag
+    }
+}
+
+removeAndSaveVgLiteStyleTag()
+
+export const vgLiteStyles = documentWithStyleTag.vgLiteDevStyleSheet.innerHTML as string
+
+if (import.meta.hot) {
+    import.meta.hot.on("vite:beforeUpdate", () => {
+        document.head.appendChild(documentWithStyleTag.vgLiteDevStyleSheet)
+    })
+
+    import.meta.hot.on("vite:afterUpdate", () => {
+        removeAndSaveVgLiteStyleTag()
+    })
+}
