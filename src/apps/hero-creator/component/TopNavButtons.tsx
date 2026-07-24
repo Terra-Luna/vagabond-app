f72e2a983f050bce409e7b8f9fcb3fa061010ad2
DESKTOP-PC\Terra <terra.shaide@gmail.com>
2026-07-24 18:30:47 -0500

    no message

--- src/apps/hero-creator/component/TopNavButtons.tsx
+++ src/apps/hero-creator/component/TopNavButtons.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react"
 import { HeroCreationSubtext } from "./HeroCreationTypography"
 
-export const TopNavButtons = ({ navButtons, subtitle }: { navButtons: ReactNode[], subtitle: string }) => {
+export const TopNavButtons = ({ navButtons, subtitle = "" }: { navButtons: ReactNode[], subtitle?: string }) => {
     if (navButtons.length === 0) return <></>
     return (
         <div className="flex items-center justify-between gap-x-4 -mt-3">
