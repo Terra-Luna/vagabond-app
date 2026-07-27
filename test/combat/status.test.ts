2c498e1a4b9f2d2c6990f4215d642121b639ec5d
Ada Lynn <adadatalynn@gmail.com>
2026-07-27 12:13:18 -0500

    Add a comment

--- test/combat/status.test.ts
+++ test/combat/status.test.ts
@@ -1,6 +1,9 @@
 import { describe, expect, test } from "@jest/globals"
 import { getCombatantStatuses } from "../../src/combat/status"
 
+// statuses are kinda strange, combatants currenty have system.statuses.statuses which is an object of [statusKey: string]: false
+// and then for each key in statuses.statuses, they MAY have a key in just system.statuses which is [statusKey: string]: true
+// I think this is bugged and system.statuses.statuses is supposed to be able to be either true or false, but who's to say
 describe('get combatant statuses', () => {
     const createCombatantWithStatuses = (activeStatuses: string[]) => {
         const activeStatusesObj = activeStatuses.reduce((acc, statusKey) => {
