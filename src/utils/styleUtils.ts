bd474e733e661c768949381f8359e8031e7d97b5
Ada Lynn <adadatalynn@gmail.com>
2026-09-03 02:07:36 -0500

    Add some comments

--- src/utils/styleUtils.ts
+++ src/utils/styleUtils.ts
@@ -2,11 +2,14 @@ import styles from "../styles/vagabond-lite.css?inline"
 export let vgLiteStyles: any = styles
 
 if (import.meta.hot) {
+    // whenever the css file hot-reloads...
     import.meta.hot.accept('../styles/vagabond-lite.css?inline', (newModule) => {
         if (newModule && newModule.default) {
+            // replace our exported variable with the new content
             vgLiteStyles = newModule.default;
             console.log('Updating tailwind styles...');
 
+            // and let all our scaduRoots know they need to update
             window.dispatchEvent(new CustomEvent('tailwind-styles-updated', {
                 detail: vgLiteStyles
             }));
@@ -14,6 +17,7 @@ if (import.meta.hot) {
     });
 }
 
+// Creates a style tag that will listen for any HMR updates
 export const createStyleTag = () => {
     const styleTag = document.createElement('style')
     styleTag.textContent = vgLiteStyles
@@ -21,6 +25,7 @@ export const createStyleTag = () => {
     return styleTag
 }
 
+// If we're running for a dev server, listens for our tailwind-styles-updated event and replaces the style tag's content with the new css
 export const listenForTailwindUpdates = (styleTag: HTMLStyleElement) => {
     if (import.meta.env.DEV) {
         const handleUpdate = (e: Event) => {
