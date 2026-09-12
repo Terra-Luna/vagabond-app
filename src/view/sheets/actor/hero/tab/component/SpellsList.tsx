a131d0d8639889db2f541cd9b994949f54b84c35
DESKTOP-PC\Terra <terra.shaide@gmail.com>
2026-09-12 03:24:58 -0500

    DEPLOY: Patch

--- src/view/sheets/actor/hero/tab/component/SpellsList.tsx
+++ src/view/sheets/actor/hero/tab/component/SpellsList.tsx
@@ -8,7 +8,6 @@ import { AbilityChatCard } from "../../../../../chat/AbilityChatCard"
 import { sendVagabondChatMessage } from "../../../../../chat/ChatCardSerializer"
 import { useContextMenu } from "../../../../../component/ContextMenu"
 import { SkillCard } from "../../../../../component/SkillCard"
-import { Tooltip } from "../../../../../component/Tooltip"
 import { useSpellcastingMenuContext } from "./spellcasting/SpellcastingMenuContext"
 
 export const SpellsList = ({ hero }: { hero: HeroDataModel }) => {
@@ -16,51 +15,46 @@ export const SpellsList = ({ hero }: { hero: HeroDataModel }) => {
     const { onCtxMenu, ContextMenu } = useContextMenu()
     const { setIsSpellcastingOpen, onSelectSpell } = useSpellcastingMenuContext()
 
-    return (
-        <div>
-            <Tooltip title={"Spells"} content={appLang.HeroSheet.context_tooltip}>
-                <div className="flex flex-col gap-x-1 gap-y-0.5">
-                {
-                    hero.spells.sort((a, b) => a.parent.name.localeCompare(b.parent.name)).map((sp: any, index: number) => (
-                        <div key={index} onContextMenu={(e) => onCtxMenu(e, [
-                            {
-                                icon: Wand2, label: appLang.HeroSheet.Magic.btnCast, action: () => {
-                                    onSelectSpell(sp._sourceId)
-                                    setIsSpellcastingOpen(true)
-                                }
-                            },
-                            {
-                                icon: MessageSquareText, label: appLang.ButtonActions.chat, action: () =>
-                                    sendVagabondChatMessage(
-                                        hero,
-                                        <AbilityChatCard
-                                            actorId={getId(hero)}
-                                            img={sp.parent.img}
-                                            title={sp.parent.name}
-                                            subtitle={spellDamageBase(sp)}
-                                            description={sp.description}
-                                        />
-                                    )
-                            }
-                        ])}>
-                            <div onClick={() => onSelectSpell(sp._sourceId)} className="w-full">
-                                <SkillCard
-                                    actor={hero.parent}
+    return (<div>
+        <div className="flex flex-col gap-x-1 gap-y-0.5">
+            {hero.spells.sort((a, b) => a.parent.name.localeCompare(b.parent.name)).map((sp: any, index: number) => (
+                <div key={index} onContextMenu={(e) => onCtxMenu(e, [
+                    {
+                        icon: Wand2, label: appLang.HeroSheet.Magic.btnCast, action: () => {
+                            onSelectSpell(sp._sourceId)
+                            setIsSpellcastingOpen(true)
+                        }
+                    },
+                    {
+                        icon: MessageSquareText, label: appLang.ButtonActions.chat, action: () =>
+                            sendVagabondChatMessage(
+                                hero,
+                                <AbilityChatCard
+                                    actorId={getId(hero)}
                                     img={sp.parent.img}
-                                    dmgType={sp.damageType}
                                     title={sp.parent.name}
-                                    subtitles={[
-                                        { label: appLang.HeroSheet.Magic.labelDmgBase, value: appLang.DamageTypes[sp.damageType] }
-                                    ]}
+                                    subtitle={spellDamageBase(sp)}
                                     description={sp.description}
                                 />
-                            </div>
-                        </div>
-                    ))
-                }
+                            )
+                    }
+                ])}>
+                    <div onClick={() => onSelectSpell(sp._sourceId)} className="w-full">
+                        <SkillCard
+                            actor={hero.parent}
+                            img={sp.parent.img}
+                            dmgType={sp.damageType}
+                            title={sp.parent.name}
+                            subtitles={[
+                                { label: appLang.HeroSheet.Magic.labelDmgBase, value: appLang.DamageTypes[sp.damageType] }
+                            ]}
+                            description={sp.description}
+                        />
+                    </div>
                 </div>
-            </Tooltip>
-            <ContextMenu />
+            ))}
         </div>
+        <ContextMenu />
+    </div >
     )
 }
